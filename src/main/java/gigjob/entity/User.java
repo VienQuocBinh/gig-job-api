4185e73bc5a40f5c62850025db97e49b3812811a
nguyen phu <100401804+PhuThanh001@users.noreply.github.com>
2023-02-16 00:02:14 +0700

    Auth

---
+++ src/main/java/gigjob/entity/User.java
@@ -0,0 +1,69 @@
+package gigjob.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+
+
+
+@Entity
+@Table(name ="User")
+public class User {
+    @Id
+    @Column(name ="")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Integer id;
+    private String username;
+    private String  password;
+    private boolean enabled;
+    
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "users_roles",
+        joinColumns =   @JoinColumn(name ="user_id"),
+        inverseJoinColumns  = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public boolean isEnabled() {
+        return enabled;
+    }
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /////abc
+}
