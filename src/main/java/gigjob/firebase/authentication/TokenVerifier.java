9a635f232ae1407abc6253b48b16fe3cd6dbe038
VienQuocBinh <realclonebinh@gmail.com>
2023-02-15 23:32:04 +0700

    Security, custom UserDetailService, Authen test

--- src/main/java/gigjob/firebase/authentication/TokenVerifier.java
+++ src/main/java/gigjob/firebase/authentication/TokenVerifier.java
@@ -7,14 +7,28 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * For verifying the id token
+ *
+ * @author Vien Binh
+ */
 @Component
 public class TokenVerifier {
     private final String GOOGLE_CLIENT_ID = "395540117439-hkbj5lk2hnrtcteihk7cv8gtsmmmbp3u.apps.googleusercontent.com";
     // 3 Authorized Parties: Android, iOS, Web apps
     private final List<String> GOOGLE_AUTHORIZED_PARTIES = List.of(new String[]{"395540117439-2pm5vil960v7n488vm6sdj7sdrv9jv5s.apps.googleusercontent.com",
             "395540117439-hriqo5e2ka4g8dotnh77a8uog4u4olh2.apps.googleusercontent.com",
-            "395540117439-hkbj5lk2hnrtcteihk7cv8gtsmmmbp3u.apps.googleusercontent.com"});
+            "395540117439-hkbj5lk2hnrtcteihk7cv8gtsmmmbp3u.apps.googleusercontent.com",
+            "395540117439-vm7157r90l3c0devegctefnll7qpr78m.apps.googleusercontent.com"});
 
+    /**
+     * Verify id token from Firebase
+     *
+     * @param idTokenString - the id token string
+     * @return {@code GoogleIdToken.Payload}
+     * @throws IOException - if an I/O error occurs
+     * @author Vien Binh
+     */
     public GoogleIdToken.Payload validate(String idTokenString) throws IOException {
         GsonFactory gsonFactory = new GsonFactory();
 //        GoogleIdTokenVerifier googleIdTokenVerifier = new GoogleIdTokenVerifier(new NetHttpTransport(), gsonFactory);
--- src/main/java/gigjob/service/UserInfoUserDetailsService.java
+++ src/main/java/gigjob/service/UserInfoUserDetailsService.java
@@ -7,21 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Component
+@Service
 public class UserInfoUserDetailsService implements UserDetailsService {
-
     @Autowired
     private AccountRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Account> userInfo = repository.findAccountByUsername(username);
-        return userInfo.map(UserInfoUserDetails::new)
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return userInfo.map(acc -> {
+                            Account account = userInfo.get();
+                            account.setPassword(passwordEncoder.encode(acc.getPassword()));
+                            return new UserInfoUserDetails(account);
+                        }
+                )
                 .orElseThrow(() -> new UsernameNotFoundException("user not found " + username));
-
     }
 }
