c76fe65f22b5a424c45cd7566472973cce353c2c
VienQuocBinh <realclonebinh@gmail.com>
2023-02-25 02:56:55 +0700

    Redis config

--- src/main/java/gigjob/GigJobApplication.java
+++ src/main/java/gigjob/GigJobApplication.java
@@ -2,10 +2,12 @@ package gigjob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class GigJobApplication {
     public static void main(String[] args) {
         SpringApplication.run(GigJobApplication.class, args);
--- src/main/java/gigjob/config/RedisConfig.java
+++ src/main/java/gigjob/config/RedisConfig.java
@@ -9,10 +9,12 @@ import org.springframework.data.redis.connection.*;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 @Setter
 public class RedisConfig {
     @Value("${redis.host}")
