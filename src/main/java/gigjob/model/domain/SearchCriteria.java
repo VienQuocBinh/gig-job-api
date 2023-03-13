648ce4e742222e068981d018c0cd34dcedfbe8ca
VienQuocBinh <realclonebinh@gmail.com>
2023-03-14 02:20:14 +0700

    Operation description, InternalServerErrorException

--- src/main/java/gigjob/model/domain/SearchCriteria.java
+++ src/main/java/gigjob/model/domain/SearchCriteria.java
@@ -9,11 +9,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchCriteria {
+    @Schema(description = "The search key words that find the jobs have the similar title to the search key")
+    private String searchKey;
+    @Schema(description = "The name of the field that used to filter",
+            example = "jobType")
     private String filterKey;
+    @Schema(description = "The value to filter", example = "1")
     private String value;
-    @Schema(name = "operation", example = "eq", description = "eq: equal" +
-            " | lt:less than" +
-            " | gt:greater than")
+    @Schema(example = "eq",
+            description = "How to filter:" +
+                    " eq: equal" +
+                    " | lt: less than" +
+                    " | gt: greater than",
+            defaultValue = "eq")
     private String operation;
     private SortCriteria sortCriteria;
 }
