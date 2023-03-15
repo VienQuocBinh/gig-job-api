0e32a5c4098832d3a6ea025455e6de3474ad4e62
Thuyen Vu <thuyenvuanh2412@gmail.com>
2023-03-15 08:12:33 +0700

    add address controller service and repo. Add some request and response defined

--- src/main/java/gigjob/service/impl/ShopServiceImpl.java
+++ src/main/java/gigjob/service/impl/ShopServiceImpl.java
@@ -1,17 +1,24 @@
 package gigjob.service.impl;
 
+import gigjob.common.meta.Role;
 import gigjob.entity.Account;
+import gigjob.entity.Address;
 import gigjob.entity.Shop;
+import gigjob.model.request.AddressRequest;
+import gigjob.model.request.NewShopProfileRequest;
 import gigjob.model.request.ShopRequest;
 import gigjob.model.response.ShopResponse;
 import gigjob.repository.AccountRepository;
 import gigjob.repository.ShopRepository;
+import gigjob.service.AddressService;
 import gigjob.service.ShopService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.webjars.NotFoundException;
 
+import java.sql.Date;
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,6 +29,7 @@ public class ShopServiceImpl implements ShopService {
     private final ShopRepository shopRepository;
 
     private final AccountRepository accountRepository;
+    private final AddressService addressService;
 
     @Override
     public List<ShopResponse> getShopList() {
@@ -39,9 +47,29 @@ public class ShopServiceImpl implements ShopService {
 
     @Override
     public ShopResponse findShopByAccountId(String accountId) {
-        var shop = shopRepository.findByAccountId(accountId);
-        if (shop.isEmpty()) throw new NotFoundException("Shop not found");
-        return modelMapper.map(shop.get(), ShopResponse.class);
+        var shop = shopRepository.findByAccountId(accountId).orElse(null);
+        if (shop == null) throw new NotFoundException("Shop not found");
+        var addresses = addressService.getAddressResponsesFromAccountId(accountId);
+        var mapper = modelMapper.map(shop, ShopResponse.class);
+        mapper.setAddresses(addresses);
+        return mapper;
+    }
 
+    @Override
+    public ShopResponse createNewShopProfile(NewShopProfileRequest request) {
+        var account = modelMapper.map(request, Account.class);
+        account.setRole(Role.SHOP);
+        account.setCreatedDate(Date.from(Instant.now()));
+        account.setUpdatedDate(Date.from(Instant.now()));
+        account.setShop(null);
+        account.setAddresses(null);
+        account = accountRepository.save(account);
+        var shop = modelMapper.map(request, Shop.class);
+        shop.setAccount(account);
+        shopRepository.save(shop);
+        var addressRequest = modelMapper.map(request.getAddress(), AddressRequest.class);
+        var address = modelMapper.map(addressRequest, Address.class);
+        addressService.save(address);
+        return findShopByAccountId(request.getAccountId());
     }
 }
