bb37941d0ebb37d69ffd5f79e54465716ce5fee5
VienQuocBinh <realclonebinh@gmail.com>
2023-03-21 21:34:49 +0700

    Cors filter

--- src/main/java/gigjob/filter/CorsFilter.java
+++ src/main/java/gigjob/filter/CorsFilter.java
@@ -13,7 +13,7 @@ public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@Nonnull HttpServletRequest request, HttpServletResponse response, @Nonnull FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
         response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
